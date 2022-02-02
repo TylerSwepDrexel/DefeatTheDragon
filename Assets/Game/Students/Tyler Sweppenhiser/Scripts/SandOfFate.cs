b75ba1ba3657f8685ed9b00f7c2252ab86ee8b37
Tysride <63009865+Tysride@users.noreply.github.com>
2022-02-02 14:46:23 -0500

    did ma card

---
+++ Assets/Game/Students/Tyler Sweppenhiser/Scripts/SandOfFate.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScriptableObjectArchitecture;
+
+public class SandOfFate : Card, ITargetSingleEnemy
+{
+    public int damage;
+    public GameObject targeterPrefab;
+    private GameObject currentTargeter;
+    public GameObject target;
+
+
+    public Vector2 maxDamage;
+    public Vector2 maxBlock;
+    public GameObject processorPrefab;
+    private GameObject currentProcessor;
+    public CallForInterjectionsGameEvent interjectionEvent;
+
+    public override void OnMouseUp()
+    {
+        if (target == null)
+        {
+        }
+        else
+        {
+            GameObject manaProcessor = Instantiate(processorPrefab);
+            manaProcessor.GetComponent<InterjectionProcessor>().startingValue = manaCost;
+            interjectionEvent.Raise(new CallForInterjections(this.gameObject, this.gameObject, InteractionType.Mana, manaProcessor.GetComponent<InterjectionProcessor>()));
+            int processedManaCost = manaProcessor.GetComponent<InterjectionProcessor>().CalculateFinalValue();
+            if (playerMana >= processedManaCost)
+            {
+                target.GetComponent<ITakeDamage>().TakeDamage(this.gameObject, currentProcessor.GetComponent<InterjectionProcessor>().CalculateFinalValue());
+                playerMana.Value -= processedManaCost;
+                discardCardEvent.Raise(this.gameObject);
+            }
+            else
+            {
+
+            }
+            Destroy(manaProcessor);
+        }
+        Deinitiate();
+    }
+
+    public void Deinitiate()
+    {
+        if (currentTargeter != null)
+        {
+            Destroy(currentTargeter);
+        }
+        if (currentProcessor != null)
+        {
+            Destroy(currentProcessor);
+        }
+        currentTargeter = null;
+        currentProcessor = null;
+        target = null;
+    }
+
+    public override void OnMouseDown()
+    {
+        base.OnMouseDown();
+        if (currentTargeter == null)
+        {
+            currentTargeter = GameObject.Instantiate(targeterPrefab);
+            currentTargeter.GetComponent<Targeter>().requester = this.gameObject;
+        }
+    }
+
+    public void ReceiveSingleEnemyTarget(GameObject receivedTarget)
+    {
+        target = receivedTarget;
+        if (receivedTarget == null)
+        {
+            if (currentProcessor != null)
+            {
+                Destroy(currentProcessor);
+            }
+        }
+        else
+        {
+            currentProcessor = GameObject.Instantiate(processorPrefab);
+            damage = Mathf.FloorToInt(Random.Range(maxDamage.x, maxDamage.y));
+            currentProcessor.GetComponent<InterjectionProcessor>().startingValue = damage;
+           
+;
+;
+            CallForInterjections currentInterjection = new CallForInterjections(this.gameObject, target, InteractionType.Damage, currentProcessor.GetComponent<InterjectionProcessor>());
+            interjectionEvent.Raise(currentInterjection);
+        }
+    }
+}
